ef72fce1e73af7f13d5ba0c46cd539dad4cac2e6
Yuechen Liu <yuechen7@siebl-0222-15.ews.illinois.edu>
2019-04-28 16:23:10 -0500

    rawFood

--- rawFood.s
+++ rawFood.s
@@ -5,6 +5,11 @@ rawFood:
   lw  $t0, 8($t1)
   blt $t0, 4, unwahsedT
   li  $a0, 2
+  sll $t2, $a0, 16
+  sw  $t2, PICKUP
+  sw  $t2, PICKUP
+  sw  $t2, PICKUP
+  sw  $t2, PICKUP
   lw  $a1, left_appliance
   lw  $a2, right_appliance
   jal appliance_location
@@ -14,6 +19,12 @@ rawFood:
 unwahsedT:
   lw  $t0, 20($t1)
   blt $t0, 4, uncutO
+  li  $t2, 3
+  sll $t2, $t2, 16
+  sw  $t2, PICKUP
+  sw  $t2, PICKUP
+  sw  $t2, PICKUP
+  sw  $t2, PICKUP
   li  $a0, 5
   lw  $a1, left_appliance
   lw  $a2, right_appliance
@@ -24,6 +35,12 @@ unwahsedT:
 uncutO:
   lw  $t0, 28($t1)
   blt $t0, 4, unWunCLettuce
+  li  $t2, 4
+  sll $t2, $t2, 16
+  sw  $t2, PICKUP
+  sw  $t2, PICKUP
+  sw  $t2, PICKUP
+  sw  $t2, PICKUP
   li  $a0, 7
   lw  $a1, left_appliance
   lw  $a2, right_appliance
@@ -34,6 +51,12 @@ uncutO:
 unWunCLettuce:
   lw  $t0, 36($t1)
   blt $t0, 4, UnchopL
+  li  $t2, 5
+  sll $t2, $t2, 16
+  sw  $t2, PICKUP
+  sw  $t2, PICKUP
+  sw  $t2, PICKUP
+  sw  $t2, PICKUP
   li  $a0, 9
   lw  $a1, left_appliance
   lw  $a2, right_appliance
@@ -44,11 +67,23 @@ unWunCLettuce:
 UnchopL:
   lw  $t0, 40($t1)
   blt $t0, 4, end
+  li  $t2, 5
+  sll $t2, $t2, 16
+  add $t2, $t2, 1
+  sw  $t2, PICKUP
+  sw  $t2, PICKUP
+  sw  $t2, PICKUP
+  sw  $t2, PICKUP
   li  $a0, 10
   lw  $a1, left_appliance
   lw  $a2, right_appliance
   jal appliance_location
+  lw  $ra, 0($sp)
+  add $sp, $sp, 4
+  jr  $ra
 end:
+  li  $v0, -1
+  li  $v1, -1
   lw  $ra, 0($sp)
   add $sp, $sp, 4
   jr  $ra
